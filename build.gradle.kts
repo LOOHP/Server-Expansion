f84f64397573972e7c9da379cba62bf1b691b060
LOOHP <jamesloohp@gmail.com>
2024-12-29 11:59:15 +0000

    Switch to ZonedDateTime

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.6-R0.1-SNAPSHOT")
-    compileOnly("me.clip:placeholderapi:2.11.3")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 tasks {
